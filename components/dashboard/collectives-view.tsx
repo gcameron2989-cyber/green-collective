ae549bb8acd1eb610fa3f53b677fb1a4f58e30dd
Graeme Cameron <gcameron2989@gmail.com>
2026-09-10 19:00:31 -0700

    Create collectives-view.tsx

---
+++ components/dashboard/collectives-view.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export function CollectivesView() {
+  return (
+    <div className="p-6 border rounded-xl bg-card">
+      <h2 className="text-xl font-bold">Sub-Collectives & Groups</h2>
+    </div>
+  )
+}
