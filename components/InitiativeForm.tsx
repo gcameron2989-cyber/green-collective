71157e67843a466789bffdfd1d27c38d55cbfbdf
Graeme Cameron <gcameron2989@gmail.com>
2026-09-18 14:49:59 -0700

    Update InitiativeForm.tsx

--- components/InitiativeForm.tsx
+++ components/InitiativeForm.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useState } from 'react'
-import { createInitiative } from '@/initiatives'
+import { createInitiative } from '@/actions/initiatives'
 
 export default function InitiativeForm() {
   const [loading, setLoading] = useState(false)
