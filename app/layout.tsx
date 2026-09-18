3b1ca45e6a0a2ed94b486c7c45f11e22855e8190
Graeme Cameron <gcameron2989@gmail.com>
2026-09-18 15:08:51 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-\import type { Metadata } from "next";
+import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/footer";
