f599c939bc1e258fc865d09444aee76e068ba636
Graeme Cameron <gcameron2989@gmail.com>
2026-09-18 14:57:41 -0700

    Create page.tsx

---
+++ app/initiatives/new/page.tsx
@@ -0,0 +1,9 @@
+import InitiativeForm from '@/components/InitiativeForm'
+
+export default function NewInitiativePage() {
+  return (
+    <main className="container mx-auto py-10 px-4">
+      <InitiativeForm />
+    </main>
+  )
+}
