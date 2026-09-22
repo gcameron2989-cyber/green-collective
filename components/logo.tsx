64baca3554fc0a64f8af0d407efb4edb53632f35
Graeme Cameron <gcameron2989@gmail.com>
2026-09-21 18:39:14 -0700

    Update logo.tsx

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import logoImage from "../public/logo.jpg";
+import logoImage from "@/public/logo.jpg";
 
 export default function Logo({ className = "" }: { className?: string }) {
   return (
